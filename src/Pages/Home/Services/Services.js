364f9f7bf381a18084e9c5f10aedff6acd561621
saifulislammunna <saifulmunna28@gmail.com>
2021-10-24 22:42:18 +0600

    update

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,9 @@ import './Services.css';
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('services.json')
+        /* server added */
+        /* fetch('services.json') */
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
